566ba218e69ba8736fa08f83b701755a2ce1833a
Jeff OConnell <jeffreytoconnell@icloud.com>
2022-07-17 18:13:45 -0700

    Finished

--- addToZero.js
+++ addToZero.js
@@ -1,15 +1,15 @@
 // Starting array
 let array = [28, 43, -12, 30, 4, 0, 12]
-let sum = 0
 // Write your solution below:
+let sum = 0
+let boolean = false;
 for (let i = 0;i < array.length; i++){
     for (let j = 0; j < array.length; j++){   
-            sum = (array[i] + array[j])
-            if (sum === 0){
-                console.log(true)    
-            }  else {
-                console.log(false)
-            }
-            
+        sum = array[i] + array[j]
+        //console.log(sum)
+        if (sum === 0){
+            boolean = true;
+        }
         }
 }
+console.log(boolean)
